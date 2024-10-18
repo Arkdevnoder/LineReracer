703aa835b74775ca0e1b1d8110760214b3752e9a
Arkdevnoder <qfybafcc@yandex.ru>
2024-10-18 13:24:55 +0500

    Benefits mask added.

--- src/Definition/Evaluator.php
+++ src/Definition/Evaluator.php
@@ -14,9 +14,27 @@ class Evaluator
 
     private int $result;
 
+    private array $masks;
+
+    private array $benefitsMask = [
+        0, 0, 0, 0,
+        500, 400, 400, 500,
+        400, 300, 300, 300,
+        200, 200, 200, 200,
+        100, 200, 200, 100,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 2000, 0
+    ];
+
     public function __construct()
     {
         $this->result = 0;
+    }
+
+    public function getMask(bool $isWhite): array
+    {
+        return $isWhite ? $this->benefitsMask : $this->getBlackBenefitsMask();
     }
 
     public function getRatio(): int
@@ -25,18 +43,51 @@ class Evaluator
         return $this->getRatioContinue();
     }
 
+    private function getBlackBenefitsMask(): array
+    {
+        foreach(array_reverse($this->benefitsMask) as $element)
+        {
+            $result[] = $element*-1;
+        }
+        return $result;
+    }
+
     private function getRatioContinue(): int
     {
-        foreach($this->getGame()->getBoard()->getPositionCollection() as $gameElement)
+        foreach($this->getGame()->getBoard()->getPositionCollection() as $key => $gameElement)
         {
-            $this->setResultBasedOnGameElement($gameElement);
+            $this->setResultBasedOnGameElement($key, $gameElement);
         }
         return $this->result;
     }
 
-    private function setResultBasedOnGameElement(GameElement $gameElement): void
+    private function setResultBasedOnGameElement(int $key, GameElement $gameElement): void
     {
+        $masks = $this->setMasks();
+        $benefit = ($gameElement instanceof Emptiness) ? 0 : $this->getBenefit($key, $gameElement);
         $this->result += ($gameElement instanceof Emptiness) ? 0
-        : ($gameElement->isWhite() ? 1000 : -1000)*($gameElement->isQueen() ? 4 : 1);
+        : ($gameElement->isWhite() ? 1000 : -1000)*($gameElement->isQueen() ? 4 : 1)+$benefit;
+    }
+
+    private function getBenefit(int $key, GameElement $gameElement): int
+    {
+        if($gameElement->isQueen())
+        {
+            return 0;
+        }
+        return $gameElement->isWhite() ? $this->masks["white"][$key] : $this->masks["black"][$key];
+    }
+
+    private function setMasks(): void
+    {
+        $this->masks = $this->getMasks();
+    }
+
+    private function getMasks(): array
+    {
+        return [
+            "white" => $this->getMask(true),
+            "black" => $this->getMask(false)
+        ];
     }
 }
